a96f6bcbee9a9c76f113b5b41607deddd55e0f64
Haakon Smith <haakonss04@gmail.com>
2023-11-11 22:50:03 +1000

    only show cascade operation to not break things

--- lib/xqflite.dart
+++ lib/xqflite.dart
@@ -5,6 +5,7 @@ library;
 
 export 'src/xqflite_base.dart';
 export 'src/schema.dart';
+export 'src/column.dart' show CascadeOperation;
 export 'src/table.dart';
 export 'src/query.dart';
 export 'src/db_table.dart';
